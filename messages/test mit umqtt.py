7d66b9455f3311d21fc9bba251d2822287172c85
Hendrik Nunn <hnunn@stud.hs-heilbron.de>
2026-06-20 19:16:14 +0200

    implemented the logic for message decoding on the pico

--- messages/test mit umqtt.py
+++ messages/test mit umqtt.py
@@ -4,7 +4,11 @@ import json as JSON
 from wlanZugriff import *
 
 # mit Wlan verbinden
-connect_to_wlan()
+# connect_to_wlan()
+
+# Verschiedene Display-Modes
+DISPLAY_MODE_LIST = ["AN", "EINE_FARBE", "FLUGZEUG", "REGENBOGEN"]
+display_mode = DISPLAY_MODE_LIST[0]
 
 # MQTT broker (public test broker)
 BROKER = "test.mosquitto.org"
@@ -20,12 +24,23 @@ def callback(topic, msg):
     try:
         msg = JSON.loads(msg)
 
-        if msg["type"] == "change_all_to_color":
-                print("hier wird der gesamte LED-Streifen auf Wert", msg["value"], "gesetzt")
-        elif msg["type"] == "change_display_mode":
-                print("hier wird der Anzeige Modus des Globe festgelegt", msg["value"])
+        if msg["type"] == "change_display_mode":
+                # beinhaltet mode einen gültigen Anzeige-Modus? <>
                 # 0 = aus, keine Anzeige
                 # 1 = fülle den gesamten Globus mit der aktuell gesetzten Farbe
+                # 2 = Färbe den Globus mit $color ein und zeige einen roten Punkt für das Flugzeug
+                # 3 = RGB Regenbogen-Farbspiel
+                if msg["mode"] >= 0 and msg["mode"] <= 3:
+                    display_mode = DISPLAY_MODE_LIST[msg["mode"]];
+                    print("hier wird der Anzeige-Modus des HoloGlobe gesetzt auf Modus #", msg["mode"])
+                else:
+                    print("Fehlerhafter Modus übertrange von change_display_mode")
+        elif msg["type"] == "change_PWM":
+                if msg["mode"] >= 0 and msg["mode"] <= 1:
+                    print("hier wird die Motorsteuerung des Picos angesprochen im Modus: ", msg["mode"])
+                    print("die Parameter lauten: ", msg["rpm"])
+        elif msg["type"] == "change_plane_position":
+               print("hier wird die Flugzeug-Position gesetzt auf x:", msg["x"], " y:", msg["y"])
         else:
                 print("Nachrichtenformat nicht erkannt")
     except Exception as e:
